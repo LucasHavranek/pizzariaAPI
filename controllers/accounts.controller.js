16b32045146e2e9f17939c4e32faa8216b80f41c
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-03-08 23:35:23 -0300

    Organização V3

--- controllers/accounts.controller.js
+++ controllers/accounts.controller.js
@@ -22,6 +22,93 @@ async function createAccount(req, res, next) {
     }
 }
 
+async function getAccounts(req, res, next) {
+    try {
+        const data = JSON.parse(await readFile(global.fileName))
+        delete data.nextId
+        res.send(data)
+        logger.info("GET /account")
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function getAccountId(req, res, next) {
+    try {
+        const data = JSON.parse(await readFile(global.fileName))
+        const buscaId = data.accounts.find(account => account.id === parseInt(req.params.id))
+        res.send(buscaId)
+        logger.info("GET /account/:id")
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function deleteAccount(req, res, next) {
+    try {
+        const data = JSON.parse(await readFile(global.fileName))
+        const buscaId = data.accounts.filter(account => account.id !== parseInt(req.params.id))
+        await writeFile(global.fileName, JSON.stringify(data, null, 2))
+        res.send(buscaId)
+        logger.info(`DELETE /account/:id - ${req.params.id}`)
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function putAccount(req, res, next) {
+    try {
+        const account = req.body
+        const data = JSON.parse(await readFile(global.fileName))
+        const index = data.accounts.findIndex(i => i.id === account.id)
+
+        if (index === -1) {
+            throw new Error("Registro não encontrado")
+        }
+
+        if (account.id == null || !account.nome || account.saldo == null) {
+            throw new Error("Nome e saldo são obrigatórios")
+        }
+
+        data.accounts[index].nome = account.nome
+        data.accounts[index].saldo = account.saldo
+        await writeFile(global.fileName, JSON.stringify(data))
+        res.send(account)
+        logger.info(`PUT /account- ${JSON.stringify(account, null, 2)}`)
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function patchAccount(req, res, next) {
+    try {
+        const account = req.body
+        const data = JSON.parse(await readFile(global.fileName))
+        const index = data.accounts.findIndex(i => i.id === account.id)
+
+        if (index === -1) {
+            throw new Error("Registro não encontrado")
+        }
+
+        if (account.id == null) {
+            throw new Error("Id é obrigatório")
+        }
+
+        data.accounts[index].nome = account.nome
+        data.accounts[index].saldo = account.saldo
+        await writeFile(global.fileName, JSON.stringify(data))
+        res.send(data.accounts[index])
+        logger.info(`PATCH /account- ${JSON.stringify(data.accounts[index])}`)
+    } catch (err) {
+        next(err)
+    }
+}
+
 export default {
-    createAccount
+    createAccount,
+    getAccounts,
+    getAccountId,
+    deleteAccount,
+    putAccount,
+    patchAccount
 }
--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -6,88 +6,11 @@ import cors from "cors"
 import accountController from "../controllers/accounts.controller.js"
 
 router.post("/", accountController.createAccount)
-
-router.get("/", async (req, res, next) => {
-    try {
-        const data = JSON.parse(await readFile(global.fileName))
-        delete data.nextId
-        res.send(data)
-        logger.info("GET /account")
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.get("/:id", async (req, res, next) => {
-    try {
-        const data = JSON.parse(await readFile(global.fileName))
-        const buscaId = data.accounts.find(account => account.id === parseInt(req.params.id))
-        res.send(buscaId)
-        logger.info("GET /account/:id")
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.delete("/:id", async (req, res, next) => {
-    try {
-        const data = JSON.parse(await readFile(global.fileName))
-        const buscaId = data.accounts.filter(account => account.id !== parseInt(req.params.id))
-        await writeFile(global.fileName, JSON.stringify(data, null, 2))
-        res.send(buscaId)
-        logger.info(`DELETE /account/:id - ${req.params.id}`)
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.put("/", async (req, res, next) => {
-    try {
-        const account = req.body
-        const data = JSON.parse(await readFile(global.fileName))
-        const index = data.accounts.findIndex(i => i.id === account.id)
-
-        if (index === -1) {
-            throw new Error("Registro não encontrado")
-        }
-
-        if (account.id == null || !account.nome || account.saldo == null) {
-            throw new Error("Nome e saldo são obrigatórios")
-        }
-
-        data.accounts[index].nome = account.nome
-        data.accounts[index].saldo = account.saldo
-        await writeFile(global.fileName, JSON.stringify(data))
-        res.send(account)
-        logger.info(`PUT /account- ${JSON.stringify(account, null, 2)}`)
-    } catch (err) {
-        next(err)
-    }
-})
-
-router.patch("/updateSaldo", async (req, res, next) => {
-    try {
-        const account = req.body
-        const data = JSON.parse(await readFile(global.fileName))
-        const index = data.accounts.findIndex(i => i.id === account.id)
-
-        if (index === -1) {
-            throw new Error("Registro não encontrado")
-        }
-
-        if (account.id == null) {
-            throw new Error("Id é obrigatório")
-        }
-
-        data.accounts[index].nome = account.nome
-        data.accounts[index].saldo = account.saldo
-        await writeFile(global.fileName, JSON.stringify(data))
-        res.send(data.accounts[index])
-        logger.info(`PATCH /account- ${JSON.stringify(data.accounts[index])}`)
-    } catch (err) {
-        next(err)
-    }
-})
+router.get("/", accountController.getAccounts)
+router.get("/:id", accountController.getAccountId)
+router.delete("/:id", accountController.deleteAccount)
+router.put("/", accountController.putAccount)
+router.patch("/updateSaldo", accountController.patchAccount)
 
 router.use((err, req, res, next) => {
     logger.error(`${req.method} ${req.baseUrl} - ${err.message}`)
